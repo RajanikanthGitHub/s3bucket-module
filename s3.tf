059e33ea4f6772d464ced9d6966c45ee3daab211
RajanikanthGitHub <rajanikanth86@gmail.com>
2021-05-31 11:40:17 +0100

    added lifecycle s3 bucket

--- s3.tf
+++ s3.tf
@@ -1,3 +1,8 @@
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "my-s3-ucket" {
   bucket = "${var.PROUECT_NAME}-bucket"
   acl    = var.ACL
@@ -8,9 +13,31 @@ resource "aws_s3_bucket" "my-s3-ucket" {
 
   server_side_encryption_configuration {
     rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = var.SSE_ALGORITHM
-        }
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
     }
   }
+
+  lifecycle_rule {
+    id      = "expire"
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 90
+    }
+
+  }
+  tags = var.STANDARD_TAGS
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,27 @@
 variable "PROUECT_NAME" {
-    description = "Provide Project Name.."
+  description = "Provide Project Name.."
 }
 
 variable "ACL" {
-    type = string
-    description = "Enable you to manage access to buckets and objects"
-    default = "private"
+  type        = string
+  description = "Enable you to manage access to buckets and objects"
+  default     = "private"
 }
 
 variable "REGION" {
-    description = "Enter your region here."  
+  description = "Enter your region here."
+  default     = "us-east-1"
 }
 
-variable "SSE_ALGORITHM" {
-    description = "Provide Server Side Encryption with SSE Algoirthm"  
+# variable "SSE_ALGORITHM" {
+#     description = "Provide Server Side Encryption with SSE Algoirthm"  
+# }
+
+variable "STANDARD_TAGS" {
+  description = "TAGS, YOU need them.."
+  default     = {
+    "Environment" = "Dev"
+    "VPC Name"    = "Main"
+    "Terraform"   = "14.3"
+  }
 }
